5431b2ec9ce065e5cbe11821945057b072e09579
shelkop <sh3lkoplyasov@gmail.com>
2022-05-20 01:11:58 +0300

    init strapi BE

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+  return {
+      ckeditor: true,
+      'entity-notes': {
+        enabled: true,
+      },
+      scheduler: {
+        enabled: true,
+        config: {
+          model: "scheduler",
+        },
+      }
+  }
+}
---
+++ src/admin/app.js
@@ -0,0 +1,7 @@
+export default {
+  config: {
+    locales: ['ru'],
+    translations: {}
+  },
+  bootstrap() {},
+};
