c2df49190b9d657206f329404e8fe20f17288540
shelkop <sh3lkoplyasov@gmail.com>
2022-07-08 01:30:51 +0300

    Tweak

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,8 +8,8 @@ cd /home/ubuntu/node-application
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 #install node modules
 npm install
