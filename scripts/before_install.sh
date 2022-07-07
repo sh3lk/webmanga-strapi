4d9aee416bee00203396b19a6401536ed3dc8598
shelkop <sh3lkoplyasov@gmail.com>
2022-07-07 22:13:33 +0300

    Tweak

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,10 +4,10 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install node 14
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/home/ec2-user/node-application"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
