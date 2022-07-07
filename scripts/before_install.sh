ff5218b55522a4997dba3b296f78660e0e6f747c
shelkop <sh3lkoplyasov@gmail.com>
2022-07-08 00:39:56 +0300

    Tweak

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14
+npm install pm2 -g
+
 DIR="/home/ubuntu/node-application"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
