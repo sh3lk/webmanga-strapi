514468c6bb41dbded54b2537ad9a48098bb2975b
shelkop <sh3lkoplyasov@gmail.com>
2022-07-07 22:55:36 +0300

    Tweak

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,11 +3,7 @@
 
 #download node, pm2 and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-
-nvm install 14
-
-npm install pm2 -g
+. ~/.nvm/nvm.sh && nvm install 14 && npm install pm2 -g
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/node-application"
