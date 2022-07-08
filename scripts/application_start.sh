4b82359ef26ba825cc78cfd6b9b2facbe204eb93
shelkop <sh3lkoplyasov@gmail.com>
2022-07-08 23:25:43 +0300

    Tweak

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,12 +12,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 #install node modules
-npm install
+#npm install
 
 #copy .env
 cp /home/ubuntu/.env .
 
 #start node app in pm2
-pm2 start server.js
-pm2 startup
-pm2 save
+#pm2 start server.js
+#pm2 startup
+#pm2 save
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 14
 npm install pm2 -g
-pm2 start /home/ubuntu/node-application/server.js
+
 DIR="/home/ubuntu/node-application"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
